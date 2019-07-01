5038219129f85f8ab140f7bc4036844782909e5d
wang033112 <wang033112@163.com>
2019-07-01 22:30:01 +0900

    Bugfix

--- app/src/main/java/com/jgo/demos/listview/PersonListActivity.java
+++ app/src/main/java/com/jgo/demos/listview/PersonListActivity.java
@@ -1,8 +1,8 @@
 package com.jgo.demos.listview;
 
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
@@ -10,7 +10,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -18,7 +17,6 @@ import android.widget.LinearLayout;
 import com.jgo.demos.R;
 import com.jgo.demos.listview.adapter.PersonRecyclerViewAdapter;
 import com.jgo.demos.util.DataManager;
-import com.jgo.demos.util.Utils;
 
 /**
  * Created by ke-oh on 2019/06/28.
@@ -33,6 +31,7 @@ public class PersonListActivity extends AppCompatActivity {
     private PersonRecyclerViewAdapter mAdapter;
     private LinearLayoutManager layoutManager;
     private LinearLayout mTopLayout;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,6 +47,8 @@ public class PersonListActivity extends AppCompatActivity {
         mRecyclerView.addOnScrollListener(new PersonScrollListener());
 
         mTopLayout = findViewById(R.id.person_list_top_layout);
+
+        mHandler = new Handler();
     }
 
     /**
@@ -57,10 +58,11 @@ public class PersonListActivity extends AppCompatActivity {
 
         private int mCurrentFirstVisiblePosition;
 
+        private int mLastAddOrRemoveViewPosition;
+
         @Override
         public void onScrollStateChanged(RecyclerView recyclerView, int newState){
             //SCROLL_STATE_IDLE == 0, SCROLL_STATE_DRAGGING == 1, SCROLL_STATE_SETTLING == 2
-            Log.d(TAG, "newState : " + newState);
         }
 
         @Override
@@ -75,33 +77,73 @@ public class PersonListActivity extends AppCompatActivity {
                 img.getLocalVisibleRect(visibleRF);
 
                 //Scroll up
-                if (dy > 0 && visibleRF.top > 0 && !mAdapter.isImgHiddenAtPosition(firstVisiblePosition)) {
-                    ImageView imageView = new ImageView(getApplicationContext());
-                    imageView.setImageDrawable(getDrawable(mAdapter.getImgSrcAtPosition(firstVisiblePosition)));
-                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-
-                    CardView cardView = new CardView(getApplicationContext());
-                    FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams((int)getResources().getDimension(R.dimen.person_list_view_img_size), (int)getResources().getDimension(R.dimen.person_list_view_img_size));
-                    //layoutParams.leftMargin = (int)getResources().getDimension(R.dimen.person_list_view_img_top_layout_left_margin);
-                    layoutParams.rightMargin = (int)getResources().getDimension(R.dimen.person_list_view_img_top_layout_right_margin);
-                    cardView.setRadius(getResources().getDimension(R.dimen.person_list_view_img_size) / 2);
-
-                    cardView.addView(imageView);
-                    mTopLayout.addView(cardView, 0, layoutParams);
-                    mAdapter.setImgHiddenAtPosition(firstVisiblePosition, true);
+                if (dy > 0 && (visibleRF.top != 0) && !mAdapter.isImgHiddenAtPosition(firstVisiblePosition)) {
+                    mHandler.post(() -> addViewTopLayout(firstVisiblePosition));
+                    mLastAddOrRemoveViewPosition = firstVisiblePosition;
                 }
 
                 //Scroll down
                 if (dy < 0 && mAdapter.isImgHiddenAtPosition(firstVisiblePosition)
-                        && (firstVisiblePosition == mCurrentFirstVisiblePosition && (visibleRF.top > 0 && visibleRF.top < img.getHeight() / 3))) {
-                    try {
-                        mTopLayout.removeViewAt(0);
-                        mAdapter.setImgHiddenAtPosition(firstVisiblePosition, false);
-                    } catch (Exception e) {
-                        Log.e(TAG, "Error at toplayout remove view");
+                        && ((firstVisiblePosition == mCurrentFirstVisiblePosition && (visibleRF.top > 0 && visibleRF.top < img.getHeight() / 3))
+                        || firstVisiblePosition < mLastAddOrRemoveViewPosition - 1)) {
+
+                    int tempPosition = mCurrentFirstVisiblePosition;
+                    while (tempPosition >= firstVisiblePosition) {
+                        final int removePosition = tempPosition;
+                        mHandler.post(() -> removeViewTopLayout(removePosition));
+                        tempPosition--;
+                        mLastAddOrRemoveViewPosition = firstVisiblePosition;
                     }
                 }
                 mCurrentFirstVisiblePosition = firstVisiblePosition;
+            }
+        }
+
+
+        /**
+         * Add imageview and cardView to toplayout.
+         *
+         * @param position the position of imageview at listview.
+         */
+        private void addViewTopLayout(int position) {
+
+            if (position <0 || position >= mAdapter.getItemCount()) {
+                return;
+            }
+
+            ImageView imageView = new ImageView(getApplicationContext());
+            imageView.setImageDrawable(getDrawable(mAdapter.getImgSrcAtPosition(position)));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+            CardView cardView = new CardView(getApplicationContext());
+            FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams((int)getResources().getDimension(R.dimen.person_list_view_img_size), (int)getResources().getDimension(R.dimen.person_list_view_img_size));
+            layoutParams.rightMargin = (int)getResources().getDimension(R.dimen.person_list_view_img_top_layout_right_margin);
+            cardView.setRadius(getResources().getDimension(R.dimen.person_list_view_img_size) / 2);
+            cardView.addView(imageView);
+
+            if (!mAdapter.isImgHiddenAtPosition(position)) {
+                mTopLayout.addView(cardView, 0, layoutParams);
+                mAdapter.setImgHiddenAtPosition(position, true);
+            }
+        }
+
+        /**
+         * Remove the view at toplayout.
+         *
+         * @param position the position of imageview at listview.
+         */
+        private void removeViewTopLayout(int position) {
+            if (position <0 || position >= mAdapter.getItemCount()) {
+                return;
+            }
+
+            try {
+                if (mAdapter.isImgHiddenAtPosition(position)) {
+                    mTopLayout.removeViewAt(0);
+                    mAdapter.setImgHiddenAtPosition(position, false);
+                }
+            } catch (Exception e) {
+                Log.e(TAG, "Error at toplayout when remove view");
             }
         }
     }
