10f16d4c7d384a8b6d0b224c411f8ef600e7e577
wang033112 <wang033112@163.com>
2019-07-03 22:07:11 +0900

    Add Media Player Action

--- app/src/main/java/com/jgo/demos/listview/adapter/VideoRecyclerViewAdapter.java
+++ app/src/main/java/com/jgo/demos/listview/adapter/VideoRecyclerViewAdapter.java
@@ -1,16 +1,22 @@
 package com.jgo.demos.listview.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.SurfaceTexture;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 import com.jgo.demos.R;
 import com.jgo.demos.listview.data.VideoData;
@@ -27,10 +33,13 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter<VideoRecycler
                                          {
     private Context mContext;
     private List<VideoData> mImageDatas = new ArrayList<>();
+    private Handler mHandler;
 
     public VideoRecyclerViewAdapter(Context context, List<VideoData> imageDatas) {
         this.mContext = context;
         this.mImageDatas = imageDatas;
+
+        mHandler = new Handler();
     }
 
     /**
@@ -68,6 +77,7 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter<VideoRecycler
 
         if (holder.mediaPlayer == null) {
             holder.mediaPlayer = new MediaPlayer();
+            holder.mediaPlayer.setOnCompletionListener(holder);
             holder.mediaPlayer.setVolume(0, 0);
             try {
                 holder.mediaPlayer.setDataSource(mContext.getApplicationContext(), Uri.parse("android.resource://com.jgo.demos/" + R.raw.jgo_demo_video));
@@ -86,18 +96,30 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter<VideoRecycler
 
 
 
-    class RecyclerHolder extends RecyclerView.ViewHolder implements TextureView.SurfaceTextureListener,
-            MediaPlayer.OnPreparedListener {
+    class RecyclerHolder extends RecyclerView.ViewHolder
+            implements TextureView.SurfaceTextureListener,
+            MediaPlayer.OnPreparedListener,View.OnClickListener,
+            MediaPlayer.OnCompletionListener,
+            View.OnTouchListener {
         FrameLayout textureViewLayout;
         TextureView textureView;
         MediaPlayer mediaPlayer;
+        ImageView playerActionImg;
+        FrameLayout playerActionLayout;
+
         boolean isMediaPlayerPrepared;
         boolean isTextureAvailable;
         SurfaceTexture surface;
 
+        @SuppressLint("ClickableViewAccessibility")
         private RecyclerHolder(View itemView) {
             super(itemView);
             textureViewLayout = itemView.findViewById(R.id.video_texture_view_layout);
+            playerActionImg = itemView.findViewById(R.id.video_list_view_action_img);
+            playerActionImg.setOnClickListener(this);
+
+            playerActionLayout = itemView.findViewById(R.id.video_list_view_action_layout);
+            playerActionLayout.setOnTouchListener(this);
         }
 
         @Override
@@ -134,8 +156,53 @@ public class VideoRecyclerViewAdapter extends RecyclerView.Adapter<VideoRecycler
 
         private void startMediaPlayer() {
             mediaPlayer.setSurface(new Surface(surface));
-            mediaPlayer.start();
+            playerActionImg.setImageDrawable(mContext.getDrawable(R.drawable.play_back_70dp));
+            //mediaPlayer.start();
         }
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.video_list_view_action_img :
+                    if (!isTextureAvailable || !isMediaPlayerPrepared) {
+                        return;
+                    }
+
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        playerActionImg.setVisibility(View.VISIBLE);
+                        playerActionImg.setImageDrawable(mContext.getDrawable(R.drawable.play_back_70dp));
+                    } else {
+                        mediaPlayer.start();
+                        playerActionImg.setVisibility(View.INVISIBLE);
+                        playerActionImg.setImageDrawable(mContext.getDrawable(R.drawable.pause_70dp));
+                    }
+                    break;
+            }
+        }
+
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            playerActionImg.setVisibility(View.VISIBLE);
+            playerActionImg.setImageDrawable(mContext.getDrawable(R.drawable.play_back_70dp));
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            playerActionImg.setVisibility(
+                    playerActionImg.getVisibility() != View.VISIBLE ? View.VISIBLE : View.INVISIBLE);
+
+            mHandler.postDelayed(() -> {
+                if (mediaPlayer.isPlaying()) {
+                    playerActionImg.setVisibility(View.INVISIBLE);
+                }
+            }, 2000);
+            return false;
+        }
+
+
+
+
     }
 }
 
