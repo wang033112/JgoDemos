b235d388271832ba46ef911476e44ae23b484221
wang033112 <wang033112@163.com>
2019-07-28 12:10:42 +0900

    circle progress view

--- app/src/main/java/com/jgo/demos/customview/DotLineActivity.java
+++ app/src/main/java/com/jgo/demos/customview/DotLineActivity.java
@@ -1,20 +1,53 @@
 package com.jgo.demos.customview;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.Button;
 
 import com.jgo.demos.R;
+import com.jgo.demos.customview.views.ProgressCircleView;
 
 /**
  * Created by ke-oh on 2019/07/23.
  *
  */
 
-public class DotLineActivity extends AppCompatActivity {
+public class DotLineActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ProgressCircleView mProgressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dotline);
+
+        mProgressView = findViewById(R.id.progress_circle);
+        findViewById(R.id.start_progress).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_progress :
+                startProgress();
+                break;
+        }
+    }
+
+    /**
+     * Start progress
+     */
+    private void startProgress() {
+        ValueAnimator animator = ValueAnimator.ofInt(0, 100);
+        animator.addUpdateListener((valueUpdate) -> {
+            mProgressView.setProgressValue((int)valueUpdate.getAnimatedValue());
+        });
+
+        animator.setInterpolator(new AccelerateInterpolator());
+        animator.setDuration(15 * 1000);
+        animator.start();
     }
 }
--- app/src/main/java/com/jgo/demos/customview/views/ProgressCircleView.java
+++ app/src/main/java/com/jgo/demos/customview/views/ProgressCircleView.java
@@ -8,6 +8,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.UiThread;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
@@ -25,6 +26,11 @@ public class ProgressCircleView extends FrameLayout {
 
     private static final int GAP_OUTER_INNER = 30;
     private static final int MARGIN_OUTER_CIRCLE = 6;
+    private static final int PAINT_STROKE_WIDTH = 3;
+    private static final int TEXT_SIZE = 80;
+
+    private int mMeasuredWidth;
+    private int mMeasureHeight;
     private int mOutCircleRadius;
     private Context mContext;
     private Paint mPrimaryColorPaint;
@@ -33,6 +39,10 @@ public class ProgressCircleView extends FrameLayout {
 
     private Canvas mInnerCanvas;
     private Bitmap mInnerBitmap;
+    private Paint mInnerCanvasCirclePaint;
+    private Paint mInnerCanvasTextPaint;
+    private float mInnerTextCenter;
+    private int mProgressValue = 0;
 
     public ProgressCircleView(@NonNull Context context) {
         super(context);
@@ -56,21 +66,21 @@ public class ProgressCircleView extends FrameLayout {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int measuredWidth = MeasureSpec.getSize(widthMeasureSpec);
-        int measureHeight = MeasureSpec.getSize(heightMeasureSpec);
+        mMeasuredWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mMeasureHeight = MeasureSpec.getSize(heightMeasureSpec);
 
-        mOutCircleRadius = Math.min(measuredWidth, measureHeight) / 2;
+        mOutCircleRadius = Math.min(mMeasuredWidth, mMeasureHeight) / 2;
 
         float left = 0.0f;
-        float right = measuredWidth;
+        float right = mMeasuredWidth;
         float top = 0.0f;
-        float bottom = measureHeight;
-        if (measuredWidth > measureHeight) {
-            left = measuredWidth / 2 - mOutCircleRadius;
-            right = measuredWidth - (measuredWidth - measureHeight) / 2;
+        float bottom = mMeasureHeight;
+        if (mMeasuredWidth > mMeasureHeight) {
+            left = mMeasuredWidth / 2 - mOutCircleRadius;
+            right = mMeasuredWidth - (mMeasuredWidth - mMeasureHeight) / 2;
         } else {
-            top = measureHeight / 2 - mOutCircleRadius;
-            bottom = measureHeight - (measureHeight - measuredWidth) / 2;
+            top = mMeasureHeight / 2 - mOutCircleRadius;
+            bottom = mMeasureHeight - (mMeasureHeight - mMeasuredWidth) / 2;
         }
         mArcRect = new RectF(
                 left + MARGIN_OUTER_CIRCLE + GAP_OUTER_INNER / 2,
@@ -79,7 +89,7 @@ public class ProgressCircleView extends FrameLayout {
                 bottom - MARGIN_OUTER_CIRCLE - GAP_OUTER_INNER / 2
         );
 
-        mInnerBitmap = Bitmap.createBitmap(mOutCircleRadius / 2, mOutCircleRadius / 2 ,Bitmap.Config.ARGB_8888);
+        mInnerBitmap = Bitmap.createBitmap(mOutCircleRadius * 3 / 4, mOutCircleRadius * 3 / 4 ,Bitmap.Config.ARGB_8888);
         mInnerCanvas = new Canvas(mInnerBitmap);
     }
 
@@ -93,6 +103,21 @@ public class ProgressCircleView extends FrameLayout {
         mGapPaint.setColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
         mGapPaint.setStrokeWidth(GAP_OUTER_INNER);
         mGapPaint.setStyle(Paint.Style.STROKE);
+
+        mInnerCanvasCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mInnerCanvasCirclePaint.setColor(ContextCompat.getColor(mContext, R.color.colorPrimary));
+        mInnerCanvasCirclePaint.setStrokeWidth(PAINT_STROKE_WIDTH);
+        mInnerCanvasCirclePaint.setStyle(Paint.Style.FILL);
+
+        mInnerCanvasTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mInnerCanvasTextPaint.setColor(ContextCompat.getColor(mContext, R.color.white));
+        mInnerCanvasTextPaint.setStrokeWidth(3);
+        mInnerCanvasTextPaint.setStyle(Paint.Style.FILL);
+        mInnerCanvasTextPaint.setTextSize(TEXT_SIZE);
+        mInnerCanvasTextPaint.setTextAlign(Paint.Align.CENTER);
+        Paint.FontMetrics fontMetrics = mInnerCanvasTextPaint.getFontMetrics();
+        mInnerTextCenter = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
+
     }
 
     @Override
@@ -101,9 +126,29 @@ public class ProgressCircleView extends FrameLayout {
 
         canvas.drawCircle(getWidth() / 2, getHeight() / 2, mOutCircleRadius - MARGIN_OUTER_CIRCLE, mPrimaryColorPaint);
         canvas.drawCircle(getWidth() / 2, getHeight() / 2, mOutCircleRadius - MARGIN_OUTER_CIRCLE - GAP_OUTER_INNER, mPrimaryColorPaint);
+        canvas.drawArc(mArcRect, -90.0f, ((float) mProgressValue / 100) * 360, false, mGapPaint);
 
-        canvas.drawArc(mArcRect, -90.0f, 90.0f, false, mGapPaint);
-        
+        mInnerCanvas.drawCircle(mInnerBitmap.getWidth() / 2, mInnerBitmap.getHeight() / 2, mInnerBitmap.getWidth() / 2 - PAINT_STROKE_WIDTH, mInnerCanvasCirclePaint);
+        mInnerCanvas.drawText(String.valueOf(mProgressValue), mInnerBitmap.getWidth() / 2, mInnerBitmap.getHeight() / 2 + mInnerTextCenter, mInnerCanvasTextPaint);
+        canvas.drawBitmap(mInnerBitmap, mMeasuredWidth / 2 - mInnerBitmap.getWidth() / 2,  mMeasureHeight /2 - mInnerBitmap.getHeight() / 2, mPrimaryColorPaint);
+    }
+
+    /**
+     *
+     * Set the progress value.
+     *
+     * @param value progress value
+     */
+    @UiThread
+    public void setProgressValue(int value) {
+        if (value < 0 ) {
+            mProgressValue = 0;
+        } else if (value > 100) {
+            mProgressValue = 100;
+        }
+
+        mProgressValue = value;
+        invalidate();
     }
 
 }
